2b368a17f19f67402b1c93b747aa80170b117cd4
sachinmewar <sachinmewar800@gmail.com>
2021-12-23 12:41:50 +0530

    reading file with FileInputStream

--- IO/File1.java
+++ IO/File1.java
@@ -1,1 +1,34 @@
 
+ import java.io.*;
+
+ public class File1{
+     public static void main(String[] args) {
+         FileInputStream fin;
+         int i = -1;
+         // Checking weather fileName is specified...
+         if(args.length != 1){
+             System.out.println("Please specify file name is console like java File1.java File1.txt");
+             return;
+         }
+
+         // Attempt to open a file...
+         try{
+             fin = new FileInputStream(args[0]);
+         }catch (FileNotFoundException e){
+             System.out.println("Cannot open file because of given exception : " + e);
+             return;
+         }
+
+         // reading from file...
+         try {
+             do{
+                 i =  fin.readLine();
+                 if(i != -1){
+                     System.out.println((char)i);
+                 }
+             }while(i != -1);
+         }catch(IOException e){
+             System.out.println(e);
+         }
+     }
+ }
